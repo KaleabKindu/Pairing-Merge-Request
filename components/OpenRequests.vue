fd26db2c06c53918bd9ada4edc5fb8b3e58a34c9
kkstarkk <kkmichaelstarkk@gmail.com>
2023-03-26 16:13:16 +0300

    implement OpenRequests component

---
+++ components/OpenRequests.vue
@@ -0,0 +1,23 @@
+<template>
+  <Requests :requests="open"/>
+</template>
+
+<script>
+import Requests from './Requests.vue';
+import {mapActions, mapState} from 'vuex';
+
+export default {
+    name:'Open',
+    components:[Requests],
+    async created(){
+        await this.fetchRequests('opened')
+    },
+    computed:{
+        ...mapState(['open'])
+    },
+    methods:{
+    ...mapActions(['fetchRequests'])
+  }
+
+}
+</script>
