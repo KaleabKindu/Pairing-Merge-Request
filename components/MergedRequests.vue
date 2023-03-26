0c7ace47a9d12c160f6fa6531b95ee41d58712ef
kkstarkk <kkmichaelstarkk@gmail.com>
2023-03-26 16:12:42 +0300

    implement MergedRequest component

---
+++ components/MergedRequests.vue
@@ -0,0 +1,24 @@
+<template>
+    <Requests :requests="merged"/>
+  </template>
+  
+  <script>
+  import {mapActions, mapState} from 'vuex';
+  import Requests from './Requests.vue';
+  
+  export default {
+      name:'Open',
+      components:[Requests],
+      async created(){
+          await this.fetchRequests('merged')
+      },
+      computed:{
+          ...mapState(['merged'])
+      },
+      methods:{
+      ...mapActions(['fetchRequests'])
+    }
+  
+  }
+  </script>
+  
