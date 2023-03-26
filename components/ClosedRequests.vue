7150196ceb4071fcc7ddc7e98ae86476a4bd46d0
kkstarkk <kkmichaelstarkk@gmail.com>
2023-03-26 15:59:59 +0300

    implement ClosedRequest component

---
+++ components/ClosedRequests.vue
@@ -0,0 +1,24 @@
+<template>
+    <Requests :requests="closed"/>
+  </template>
+  
+  <script>
+import {mapActions, mapState} from 'vuex';
+  import Requests from './Requests.vue';
+  
+  export default {
+      name:'Closed',
+      components:[Requests],
+      async created(){
+          await this.fetchRequests('closed')
+      },
+      computed:{
+          ...mapState(['closed'])
+      },
+      methods:{
+      ...mapActions(['fetchRequests'])
+    }
+  
+  }
+  </script>
+  
