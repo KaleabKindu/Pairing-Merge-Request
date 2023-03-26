c7a86a0687a3b9cf4e7ff99445155c319de6df27
kkstarkk <kkmichaelstarkk@gmail.com>
2023-03-26 15:52:21 +0300

    implement AllRequest component

---
+++ components/All.vue
@@ -0,0 +1,24 @@
+<template>
+    <Requests :requests="all"/>
+  </template>
+  
+  <script>
+  import Requests from './Requests.vue';
+  import {mapActions, mapState} from 'vuex';
+  
+  export default {
+      name:'All',
+      components:[Requests],
+      async created(){
+          await this.fetchRequests('all')
+      },
+      computed:{
+          ...mapState(['all'])
+      },
+      methods:{
+      ...mapActions(['fetchRequests'])
+    }
+  
+  }
+  </script>
+  
